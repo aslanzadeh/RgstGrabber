010f37dd8b6082c9ae30c210d0d552860734f455
Reza Aslanzadeh <raslanzadeh@hard-line.com>
2020-10-03 20:32:05 -0400

    initial commit

---
+++ src/libRgstGrabber/libRgstGrabber/Interfaces/ICamera.cpp
@@ -0,0 +1,9 @@
+//
+// Created by reza on 2020-10-03.
+//
+
+#include "ICamera.h"
+
+namespace RgstGrabber {
+
+}
---
+++ src/libRgstGrabber/libRgstGrabber/Interfaces/ICamera.h
@@ -0,0 +1,15 @@
+//
+// Created by reza on 2020-10-03.
+//
+
+#pragma once
+
+namespace RgstGrabber {
+    class ICamera {
+
+    };
+}
+
+
+
+
